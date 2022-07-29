9ed58df1943faeda936413bad70ed53368681a9e
Temitope3665 <aroyewontope@gmail.com>
2022-07-29 23:07:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import patternDivider from './images/pattern-divider-desktop.svg';
+import dice from './images/icon-dice.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import spinLoading from './images/spin.png';
 
 function App() {
+  const [advice, setAdvice] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const url = 'https://api.adviceslip.com/advice';
+
+  const getAdvice = () => {
+    setLoading(true);
+    axios.get(`${url}`)
+    .then((res) => setAdvice(res.data.slip))
+    .catch((err) => console.log(err))
+    .finally(() => setLoading(false))
+  };
+
+  useEffect(() => {
+    getAdvice()
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <main className="App wrapper">
+        <div className='content-box'>
+          <p className='advice-no'>{`ADVICE #${loading ? '..' :advice.id}`}</p>
+          {loading ? <img className='loading' width="60px" src={spinLoading} alt="spin" /> : 
+            <p className='advice-text'>{advice.advice}</p>
+          }
+          <img className='divider' src={patternDivider} alt="divider" />
+          <div className='dice-wrapper' onClick={getAdvice}>
+            <img src={dice} alt="dice" />
+          </div>
+        </div>
+      </main>
+    </body>
   );
 }
 
